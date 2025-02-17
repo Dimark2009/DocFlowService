94bb9efa80a2917c30670bfa41d80e0809c24afd
Dimark2009 <Dimark2008@gmail.com>
2025-02-17 18:20:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-import telebot  # Подключение библиотеки для работы с Telegram
-from telebot import types  # для указание типов
+from flask import Flask, request
+import telebot
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-bot = telebot.TeleBot(os.environ.get("BOT_TOKEN"))  # Добавление токена для подключения к боту
+bot = telebot.TeleBot(os.environ.get("BOT_TOKEN"))  
 
-@bot.message_handler(commands=['start']) #Декоратор для обработки команды /start
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    json_str = request.get_data(as_text=True)
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return '', 200
+
+@app.route('/set_webhook', methods=['GET'])
+def set_webhook():
+    webhook_url = os.environ.get("WEBHOOK_URL") 
+    bot.remove_webhook()
+    bot.set_webhook(url=webhook_url)
+    return "Webhook set!", 200
+
+@bot.message_handler(commands=['start']) 
 def start(message):
     bot.send_message(message.chat.id, 'Вас приветствует мастер-бот!\nЗагружайте и просматривайте документы в пару кликов:)\nНачать: /enter')
 
-@bot.message_handler(commands=['enter']) #Декоратор для обработки команды /enter
+@bot.message_handler(commands=['enter'])  
 def main(message):
     markup = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton('Загрузить', callback_data='download')
@@ -19,23 +35,22 @@ def main(message):
     markup.row(btn1, btn2)
     bot.send_message(message.chat.id, 'Выберите команду', reply_markup=markup)
 
-@bot.message_handler(commands=['info']) #Декоратор для обработки команды /info
-def main(message):
+@bot.message_handler(commands=['info']) 
+def info(message):
     bot.send_message(message.chat.id, 'Master bot V1.0.')
 
-@bot.message_handler(content_types=['document']) #Функция сохранения документов в папку "documents"
+@bot.message_handler(content_types=['document'])  
 def get_document(message):
     if message.document:
         filename = message.document.file_name
         file_info = bot.get_file(message.document.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
-        file_path = os.path.join('documents', filename) #Выбор папки для сохранения
+        file_path = os.path.join('documents', filename)  # Выбор папки для сохранения
         with open(file_path, 'wb') as file:
             file.write(downloaded_file)
             bot.reply_to(message, 'Документ сохранен!\nВернуться на начальный экран? /enter')
     else:
         bot.send_message(message.chat.id, "Не удалось сохранить документ")
-
 
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
@@ -61,4 +76,5 @@ def doc_name(message):
     else:
         bot.send_message(message.chat.id, "Файл не найден. Попробуйте еще раз\nВернуться на начальный экран? /enter")
 
-bot.polling(non_stop=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Запуск Flask приложения
